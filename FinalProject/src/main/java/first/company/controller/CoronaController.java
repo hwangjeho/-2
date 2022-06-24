105b8db0ce553a92e49896b67afb7b4724bc9ee2
jehohwang <jeho0809@naver.com>
2022-06-24 10:32:57 +0900

    코로나 API 사용한 컨트롤러 수정

--- FinalProject/src/main/java/first/company/controller/CoronaController.java
+++ FinalProject/src/main/java/first/company/controller/CoronaController.java
@@ -3,9 +3,7 @@ package first.company.controller;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLEncoder;
 
@@ -21,6 +19,10 @@ public class CoronaController {
 		StringBuilder urlBuilder = new StringBuilder("http://apis.data.go.kr/1790387/covid19CurrentStatusKorea/covid19CurrentStatusKoreaJason");
 		String serviceKey = "vbU228Siu2sd29G4M9QUcXXlv8e%2BUiT%2BzeeSmyr08Qu%2BxagpVqJyV0Tk3SJznaZ%2Bvj9%2F9FGAcqk7nKq5YgO7GA%3D%3D";
 		urlBuilder.append("?" + URLEncoder.encode("serviceKey","UTF-8") + "=" + serviceKey);
+		urlBuilder.append("&" + URLEncoder.encode("pageNo","UTF-8") + "=" + URLEncoder.encode("1", "UTF-8")); /*페이지번호*/
+	    urlBuilder.append("&" + URLEncoder.encode("numOfRows","UTF-8") + "=" + URLEncoder.encode("10", "UTF-8")); /*한 페이지 결과 수*/
+	    urlBuilder.append("&" + URLEncoder.encode("startCreateDt","UTF-8") + "=" + URLEncoder.encode("20200310", "UTF-8")); /*검색할 생성일 범위의 시작*/
+	    urlBuilder.append("&" + URLEncoder.encode("endCreateDt","UTF-8") + "=" + URLEncoder.encode("20200315", "UTF-8")); /*검색할 생성일 범위의 종료*/
 		URL url = new URL(urlBuilder.toString());
 		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
 		conn.setRequestMethod("GET");
